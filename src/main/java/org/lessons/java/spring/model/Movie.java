3f071d815c5336e5317300d37e5de7350c6a0066
AnastasiaPro <165699559+AnastasiaProshy@users.noreply.github.com>
2024-08-28 20:43:48 +0200

    Initial Commit - Adding the Basic
    the basic Movie class definition including fields and the constructor

---
+++ src/main/java/org/lessons/java/spring/model/Movie.java
@@ -0,0 +1,15 @@
+package org.lessons.java.spring.model;
+
+public class Movie 
+{
+	
+	private int id;
+	private String title;
+	
+	
+	public Movie (int id, String title)
+	{
+		this.id = id;
+		this.title = title;
+	}
+	
